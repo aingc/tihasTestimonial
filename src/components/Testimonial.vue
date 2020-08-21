c21eed8ba4f87f94e93a628f02d8103a99650898
Corey <corey.aing@tiger-sheep.com>
2020-08-21 15:58:59 -0700

    Resized delimiters

--- src/components/Testimonial.vue
+++ src/components/Testimonial.vue
@@ -4,47 +4,56 @@
       <v-app>
         <v-container>
           <v-sheet class="mx-auto">
-            <v-expand-transition>
-              <v-sheet
-                v-if="currentTestimonial != null"
-                color="grey lighten-4"
-              >
-                <v-card
-                  class="mx-lg-auto yellow lighten-1 pa-2"
-                  outlined>
-                  <div></div>
-                  <v-avatar color="grey" size="132">
-                    <span class="white--text headline">{{testimonialList[currentTestimonial].id}}</span>
-                  </v-avatar>
-                  <v-card-text class="blue--text">
-                    <p>{{testimonialList[currentTestimonial].name}}</p>
-                    <p>{{testimonialList[currentTestimonial].company}}</p>
-                    <p>{{testimonialList[currentTestimonial].location}}</p>
-                  </v-card-text>
-                </v-card>
-              </v-sheet>
-            </v-expand-transition>
-            <v-slide-group
-              class="pa-4"
-              v-model="currentTestimonial"
-              mandatory
-            >
-              <v-slide-item
-                v-bind:key="testimonial.id"
-                v-for="testimonial in testimonialList"
-                v-slot:default="{ active, toggle }"
-              >
-                <v-row justify="center">
-                  <v-col>
-                    <v-avatar
-                      :color="active ? '#2A4898' : '#FCE700'"
-                      @click="toggle"
+            <v-row justify="center">
+              <v-col class="col-4">
+                <v-expand-transition>
+                  <v-sheet
+                    v-if="currentTestimonial != null"
+                  >
+                    <v-card
+                      class="mx-lg-auto yellow lighten-1 pa-2"
+                      outlined
                     >
-                    </v-avatar>
-                  </v-col>
-                </v-row>
-              </v-slide-item>
-            </v-slide-group>
+                      <div></div>
+                      <v-avatar color="grey" size="132">
+                        <span class="white--text headline">{{testimonialList[currentTestimonial].id}}</span>
+                      </v-avatar>
+                      <v-card-text class="blue--text">
+                        <p>{{testimonialList[currentTestimonial].name}}</p>
+                        <p>{{testimonialList[currentTestimonial].company}}</p>
+                        <p>{{testimonialList[currentTestimonial].location}}</p>
+                      </v-card-text>
+                    </v-card>
+                  </v-sheet>
+                </v-expand-transition>
+              </v-col>
+            </v-row>
+            <v-row justify="center">
+              <v-col class="col-2">
+                <v-slide-group
+                  class="pa-4"
+                  v-model="currentTestimonial"
+                  mandatory
+                >
+                  <v-slide-item
+                    v-bind:key="testimonial.id"
+                    v-for="testimonial in testimonialList"
+                    v-slot:default="{ active, toggle }"
+                  >
+                    <v-row>
+                      <v-col>
+                        <v-avatar
+                          :size="24"
+                          :color="active ? '#2A4898' : '#FCE700'"
+                          @click="toggle"
+                        >
+                        </v-avatar>
+                      </v-col>
+                    </v-row>
+                  </v-slide-item>
+                </v-slide-group>
+              </v-col>
+            </v-row>
           </v-sheet>
         </v-container>
       </v-app>
